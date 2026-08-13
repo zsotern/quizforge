0c4c584780d3feb9451de5bde765cfa76caad3f3
Zsótér Nikoletta <h270925@stud.u-szeged.hu>
2026-08-13 12:23:28 +0200

    feat(server): POST /api/quizzes with transaction, GET api/quizzes/:id

--- server/server.js
+++ server/server.js
@@ -6,16 +6,6 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
-async function testConnection() {
-    const client = await pool.connect();
-    const result = await client.query('SELECT NOW()');
-    console.log(result.rows[0]);
-    client.release();
-}
-
-testConnection();
-
-
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -44,5 +34,62 @@ app.get('/api/quizzes', async (req, res) => {
             client.release();
         }
     }
+});
 
-})
+app.get('/api/quizzes/:id', async (req, res) => {
+    let client;
+    try{
+        client = await pool.connect();
+        const id = req.params.id;
+        const quizResult = await client.query('SELECT*FROM quizzes WHERE id=$1', [id]);
+        const quiz = quizResult.rows[0];
+        const questions = await client.query('SELECT*FROM questions WHERE quiz_id=$1', [quiz.id]);
+        const questionsWithAnswers = await Promise.all(questions.rows.map(async question => {
+            const answerResult = await client.query('SELECT*FROM answers WHERE question_id=$1', [question.id]);
+            question.answers = answerResult.rows;
+            return question;
+        }));
+        quiz.questions = questionsWithAnswers;
+        res.json(quiz);
+    }catch(err){
+        console.error(err);
+        res.status(500).json('Error getting quizzes');
+    } finally {
+        if(client){
+            client.release();
+        }
+    }
+});
+
+app.post('/api/quizzes', async (req, res) => {
+    const { id, title, allowBackTrack, questions } = req.body;
+
+    let client;
+    try {
+        client = await pool.connect();
+        await client.query('BEGIN');
+        await client.query('INSERT INTO quizzes (id, title, allow_back_track) ' +
+            'VALUES ($1,$2,$3)', [id, title, allowBackTrack]);
+        for(const question of questions){
+            await client.query('INSERT INTO questions (id, text, quiz_id, explanation) ' +
+                'VALUES ($1,$2,$3, $4)', [question.id, question.text, id, question.explanation]);
+            for(const answer of question.answers){
+                const isCorrect = question.correctAnswerIds.includes(answer.id);
+                await client.query('INSERT INTO answers (id, text, question_id, is_correct) ' +
+                    'VALUES ($1,$2,$3, $4)', [answer.id, answer.text, question.id, isCorrect]);
+            }
+        }
+        await client.query('COMMIT');
+        res.status(201).json('Successfully created quiz');
+    } catch (err) {
+        if(client){
+            await client.query('ROLLBACK');
+        }
+        console.error(err);
+        res.status(500).json('Error creating quiz');g
+    } finally {
+        if (client) {
+            client.release();
+        }
+    }
+});
